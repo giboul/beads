be90d20083b7a80c1c31f4a8f2dfe592814d9aba
giboul <axel.giboulot@epfl.ch>
2026-01-07 17:03:12 +0100

    it now runs

---
+++ constant/writeG.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from argparse import ArgumentParser
+from pathlib import Path
+from math import atan, sin , cos
+
+def parse_args():
+    p = ArgumentParser()
+    p.add_argument("slope", type=float)
+    p.add_argument("-g", default=9.81, type=float)
+    return p.parse_args()
+
+args = parse_args()
+
+gx = + sin(args.slope) * args.g
+gz = - cos(args.slope) * args.g
+
+template = (Path("constant") / "gTemplate").read_text()
+(Path("constant") / "g").write_text(template % f"{gx:3f} 0 {gz:.3f}")
+
